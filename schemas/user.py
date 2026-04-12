c09404a8d6e74646af6bbb8a881b19ef76c9c4f7
Beka Eli <bekaeli27@gmail.com>
2026-04-12 19:39:45 +0300

    allowing nullable hospital_id for users

--- schemas/user.py
+++ schemas/user.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel
 from models.user import UserRoles
+from typing import Optional
 class UserCreate(BaseModel):
     user_id:str
     full_name:str
-    hospital_id:str
+    hospital_id:Optional[str]=None
     role:UserRoles
     password:str
 class UserResponse(BaseModel):
     user_id:str
     full_name:str
-    hospital_id:str
+    hospital_id:Optional[str]=None
     role:UserRoles
     class Config:
         from_attributes=True
