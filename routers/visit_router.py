ced66316a920e81131ad4b1d7ed833038c20f019
Beka Eli <bekaeli27@gmail.com>
2026-04-30 13:39:49 +0300

    staff

--- routers/visit_router.py
+++ routers/visit_router.py
@@ -9,20 +9,20 @@ from services.visit import (create_visit,get_visit_by_id,get_all_visits_for_pati
 from core.permissions import require_roles
 router=APIRouter(prefix="/visits",tags=["Visits"])
 @router.post("/",response_model=ResponseModel)
-def register_visit(data:VisitCreate,db:Session=Depends(get_db),current_user:User=Depends(get_user_object),_:dict=Depends(require_roles("ADMIN","DOCTOR","NURSE"))):
+def register_visit(data:VisitCreate,db:Session=Depends(get_db),current_user:User=Depends(get_user_object),_:dict=Depends(require_roles("ADMIN","DOCTOR","NURSE","STAFF"))):
     try:
         return create_visit(db,data,current_user)
     except ValueError as e:
         raise HTTPException(400,detail=str(e))
 @router.get("/{patient_id}/{visit_id}",response_model=VisitResponse)
-def read_visit(patient_id:str,visit_id:str,db:Session=Depends(get_db),current_user:dict=Depends(require_roles("ADMIN","DOCTOR","NURSE"))):
+def read_visit(patient_id:str,visit_id:str,db:Session=Depends(get_db),current_user:dict=Depends(require_roles("ADMIN","DOCTOR","NURSE","STAFF"))):
     visit=get_visit_by_id(db,patient_id,visit_id)
     db.refresh(visit)
     if not visit:
         raise HTTPException(404,detail="Visit not found")
     return visit
 @router.get("/{patient_id}",response_model=list[VisitResponse])
-def read_all_visits(patient_id:str,db:Session=Depends(get_db),current_user:dict=Depends(require_roles("ADMIN","DOCTOR","NURSE"))):
+def read_all_visits(patient_id:str,db:Session=Depends(get_db),current_user:dict=Depends(require_roles("ADMIN","DOCTOR","NURSE","STAFF"))):
     return get_all_visits_for_patient(db,patient_id)
 @router.put("/{visit_id}")
 def update_visit(
