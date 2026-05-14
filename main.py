6e9c9ae101c0a3ecc14aee8905c9b881ed63530a
Beka Eli <bekaeli27@gmail.com>
2026-05-14 21:51:04 +0300

    routing

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from routers.dashboard_router import router as dashboard_router
 from routers.pharmacy_router import router as pharmacy_router
 from routers.pharmacysales_router import router as pharmacysales_router
 from routers.inventory_router import router as inventory_router
-from routers.intelligency_router import router as intelligency_router
+from routers.intelligence_router import router as intelligence_router
 from core.config import settings
 
 setup_logging()
@@ -67,7 +67,7 @@ app.include_router(dashboard_router)
 app.include_router(pharmacy_router)
 app.include_router(pharmacysales_router)
 app.include_router(inventory_router)
-app.include_router(intelligency_router)
+app.include_router(intelligence_router)
 
 
 
